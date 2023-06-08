20f81b60191dd595b9e6967d55a1b07b00114a9c
hmeressa-IE <135103661+hmeressa-IE@users.noreply.github.com>
2023-06-08 14:59:02 +0300

    added more files

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const jenkinsTest = (username) =>{
 console.log("This is testing from jenkins and build");
 console.log(`This is my username : ${username}`)
+  console.log(`This is my username : ${username}`)
+  console.log(`This is my username : ${username}`)
+  console.log(`This is my username : ${username}`)
 }
 jenkinsTest("hmeressa");
