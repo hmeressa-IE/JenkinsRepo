1fe2212e4f0ab1834c29b60cbb21ceaf704e8b4f
Hiluf Meressa <hmkahsay@gmail.com>
2023-06-10 16:17:41 +0300

    message

--- shell.sh
+++ shell.sh
@@ -1,2 +1,2 @@
-#!/bin/sh —
+#!/bin/bash
 scp -r -i C:\ProgramData\Jenkins\jenkins_ssh :\ProgramData\Jenkins\.jenkins\workspace\JenkinsTest icladu0s@icladdis.com:/home2/icladu0s/gsharewcd.icladdis.com
